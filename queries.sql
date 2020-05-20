172cc3b7fd12bd3d18913d6f70e58ec64452ca9d
kyryllparolys <futuregoooogler@gmail.com>
2020-05-20 20:25:05 +0300

    Finished second task.

--- queries.sql
+++ queries.sql
@@ -1,9 +1,31 @@
-select
+-- First query
+SELECT
   titles.title, employees.first_name, employees.last_name, salaries.salary
-from titles
-inner join employees on
+FROM titles
+INNER JOIN employees ON
 	titles.emp_no=employees.emp_no
-inner join salaries on
+INNER JOIN salaries ON
 	employees.emp_no=salaries.emp_no
-where titles.title like "%Manager%";
+WHERE titles.title LIKE "%Manager%";
 
+-- Second query
+SELECT
+	e.first_name, e.last_name, e.hire_date, DATEDIFF(CURDATE(), e.hire_date) as at_company,
+	t.title,
+	d_emp.dept_no, d.dept_name
+FROM
+	employees e
+INNER JOIN titles t on e.emp_no = t.emp_no
+INNER JOIN dept_emp d_emp on t.emp_no = d_emp.emp_no
+INNER JOIN departments d on d_emp.dept_no = d.dept_no;
+
+-- Third query
+SELECT
+	d.dept_no,
+	COUNT(*),
+	SUM(s.salary)
+FROM
+	departments d
+INNER JOIN dept_emp de on d.dept_no = de.dept_no
+INNER JOIN salaries s on de.emp_no = s.emp_no
+GROUP BY d.dept_no;
